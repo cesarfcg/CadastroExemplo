ada19532f0c5586fa5dc74d423079aa50d7d8043
Fernando César <fernandocesar4141@gmail.com>
2025-07-30 10:05:46 -0300

    first commit

---
+++ CadastroInicial/src/main/java/com/example/CadastroInicial/CadastroInicialApplication.java
@@ -0,0 +1,13 @@
+package com.example.CadastroInicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroInicialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroInicialApplication.class, args);
+	}
+
+}
