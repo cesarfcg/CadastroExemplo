9d7205897b17139a10905f15b7413bc21814d273
Fernando César <fernandocesar4141@gmail.com>
2025-08-18 14:58:52 -0300

    Criação de entidade no banco de dados a partir do UsuarioModel

---
+++ CadastroInicial/src/main/java/com/example/CadastroInicial/UsuarioModel.java
@@ -0,0 +1,47 @@
+package com.example.CadastroInicial;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class UsuarioModel {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public UsuarioModel() {
+    }
+
+    public UsuarioModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
